acea58bb4b8d80b8eba236951d4d0a783cfae62d
Michael Moser <moser.michael@gmail.com>
2021-08-21 16:29:11 +0300

    bidi support

---
+++ test-bidi.py
@@ -0,0 +1,15 @@
+import util
+
+# visual studio code is a bit messad up with bidi support. 
+# open issue since 2016 https://github.com/microsoft/vscode/issues/11770
+# a workaround is to put bidi text into separate variables
+
+heb="שלום לכולם"
+heb2="שלום world"
+
+util.printb( heb, 42 )
+util.printb( heb2, 42 )
+
+#print(heb)
+#print(heb2)
+
